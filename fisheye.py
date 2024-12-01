5f04cd420c09505071858a51870ae2d124db8c16
Johnafriedman <john@innovationsforlearning.org>
2024-12-01 12:29:34 -0600

    adding focal length, command line parameters

--- fisheye.py
+++ fisheye.py
@@ -3,11 +3,11 @@ import numpy as np
 import pygame
 import sys
 
-def apply_fisheye(image, c_x, c_y, strength=1.0,):
+def apply_fisheye(image, c_x, c_y, f_x, f_y, strength=1.0):
     """Applies a fisheye effect to an image using cv2.fisheye.initUndistortRectifyMap."""
     height, width = image.shape[:2]
-    K = np.array([[width, 0, c_x],
-                  [0, width, c_y],
+    K = np.array([[f_x, 0, c_x],
+                  [0, f_y, c_y],
                   [0, 0, 1]], dtype=np.float32)
     D = np.array([strength, strength, 0, 0], dtype=np.float32)
 
@@ -26,6 +26,10 @@ def display_image_with_pygame(image):
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Fisheye Transformation")
 
+    # Initialize font
+    pygame.font.init()
+    font = pygame.font.SysFont('Arial', 24)
+
     # Convert the image to a format suitable for Pygame
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image_surface = pygame.surfarray.make_surface(image_rgb)
@@ -35,11 +39,17 @@ def display_image_with_pygame(image):
     strength = -2.5  # Initial strength
     mouse_down = False
     last_mouse_x = None
+    last_mouse_y = None
     MAX_STRENGTH = -50
-    MIN_STRENGTH = -1
+    MIN_STRENGTH = 50
     INC_STRENGTH = 0.1
+    MAX_FOCAL_LENGTH = width * 10
+    MIN_FOCAL_LENGTH = width // 10
+    INC_FOCAL_LENGTH = 0.1
     c_x = width // 2
     c_y = height // 2
+    f_x = width
+    f_y = width
 
     while running:
         for event in pygame.event.get():
@@ -50,6 +60,7 @@ def display_image_with_pygame(image):
                 if event.button == 1:  # Left mouse button
                     mouse_down = True
                     last_mouse_x = event.pos[0]
+                    last_mouse_y = event.pos[1]
                 elif event.button == 4:  # Mouse wheel up
                     strength = min(strength + 0.1, MIN_STRENGTH)
                 elif event.button == 5:  # Mouse wheel down
@@ -60,19 +71,32 @@ def display_image_with_pygame(image):
             elif event.type == pygame.MOUSEMOTION:
                 if mouse_down:
                     dx = event.pos[0] - last_mouse_x
+                    dy = event.pos[1] - last_mouse_y
+
                     strength += dx * INC_STRENGTH
                     strength = max(min(strength, MIN_STRENGTH), MAX_STRENGTH)
+                    
+                    f_x += dy * INC_FOCAL_LENGTH
+                    f_x = max(min(f_x, MAX_FOCAL_LENGTH), MIN_FOCAL_LENGTH)
+                    f_y = f_x
+                    
                     last_mouse_x = event.pos[0]
+                    last_mouse_y = event.pos[1]
+
+
+                    print(f" dy: {dy}, last_mouse_y: {last_mouse_y}, f_x: {f_x}")
+
                 else:
                     c_x = event.pos[1]
                     c_y = event.pos[0]
 
                 # Regenerate the fisheye image with the new strength
-                fisheye_image = apply_fisheye(image, c_x, c_y, strength)
+                fisheye_image = apply_fisheye(image, c_x, c_y, f_x, f_y, strength)
                 image_rgb = cv2.cvtColor(fisheye_image, cv2.COLOR_BGR2RGB)
                 image_surface = pygame.surfarray.make_surface(image_rgb)
 
         screen.blit(image_surface, (0, 0))
+
         pygame.display.flip()
 
     pygame.quit()
@@ -85,17 +109,25 @@ def main(image_path):
         print(f"Error: Unable to load image {image_path}")
         return
 
-# scale image down proportionately so the width is 1280
-    scale_percent = 1280 / image.shape[0] * 100
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
+    # set size of image. algorithem generates a square image
+    size = 1600
+    dim = (size, size)
     image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-
 
     # Display the transformed image using Pygame
     display_image_with_pygame(image)
 
 if __name__ == "__main__":
-    image_path = "input/Photos-001/PXL_20240724_165101814~3.jpg"  # Replace with the path to your image file
+    # get path from commandline if available
+    if len(sys.argv) > 1:
+        image_path = sys.argv[1]
+
+    else:
+        image_path = "input/Photos-001/PXL_20240724_165101814~3.jpg"  # Replace with the path to your image file
+
+    if len(sys.argv) > 2:
+        output_dir = sys.argv[2]
+    else:
+        output_dir = "output/"
+
     main(image_path)
