34bc3f61a5f9659ee77f2088c15127cc301ccc50
Johnafriedman <john@innovationsforlearning.org>
2024-12-02 16:09:41 -0600

    adding esc to exit and s to save

--- meta_pixel_controller.py
+++ meta_pixel_controller.py
@@ -866,7 +866,7 @@ class Controller(tk.Tk):
         # execute fisheye.py in the terminal with commandline parameters        
         # Functionality to generate fisheye
         self.button_fisheye.config(state="disabled")
-        subprocess.Popen(["python", "mirek-generative/fisheye.py", self.model.input_path, self.model.output_dir, self.model.image_name, self.model.image_date])
+        subprocess.Popen(["python", "mirek-generative/fisheye.py", self.model.input_path, self.model.output_dir])
         self.button_fisheye.config(state="normal")
 
     def exit(self):
